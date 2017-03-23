3deacecac087466ffc6ca269c5627145406c187e
Eder Eduardo <eder.esilva@gmail.com>
2017-03-23 19:50:39 -0300

    added json

--- app/routes/sac.js
+++ app/routes/sac.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('sac');
-    return [{ title: 'Test', body: 'Corpo'}];
+    return this.store.findAll('sac');
   }
 });
