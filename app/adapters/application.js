415226c21f6ef52197684f69a7b7a5c7a7dd08a9
Eder Eduardo <eder.esilva@gmail.com>
2017-03-24 18:57:19 -0300

    removed files unuseds and fix adapters

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.RESTAdapter.extend({
+export default DS.JSONAPIAdapter.extend({
   //host: 'https://emberpoc-762c2.firebaseio.com',
   buildURL: () => {
     return `https://emberpoc-762c2.firebaseio.com/.json`;
   }
 });
-
