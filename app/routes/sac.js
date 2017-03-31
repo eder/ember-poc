e4f79a031b26e45bf1dc44f392c32ad3eb2c291c
Eder Eduardo <eder.esilva@gmail.com>
2017-03-31 19:25:00 -0300

    AMP Works

--- app/routes/sac.js
+++ app/routes/sac.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
+const { set } = Ember;
+
 export default Ember.Route.extend({
   amp: Ember.inject.service(),
+  headData: Ember.inject.service(),
   model() {
     return this.store.findAll('sac');
   },
 
-  afterModel(sac) {
-    //this.get('amp').set('canonicalUrl', s);
-    //this.get('amp').set('title', sac.get('title'));
-    //this.get('amp').registerExtension('twitter');
-    console.log(sac);
-    }
+  afterModel() {
+    //this.get('amp').set('canonicalUrl', 'http://');
+    //this.get('amp').set('title', 'Works!!');
+    this.get('amp').registerExtension('twitter');
+    set(this, 'headData.title', 'Demo App');
+  }
 });
