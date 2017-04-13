ca3c09f0ed0f7743a17c4544d7171aec8bd59eba
Eder Eduardo <eder.esilva@gmail.com>
2017-04-13 17:50:31 -0300

    Fix inject ember-amp

--- app/routes/sac.js
+++ app/routes/sac.js
@@ -1,18 +1,16 @@
 import Ember from 'ember';
 
-const { set } = Ember;
-
 export default Ember.Route.extend({
   amp: Ember.inject.service(),
-  headData: Ember.inject.service(),
+  fastboot: Ember.inject.service('fastboot'),
   model() {
     return this.store.findAll('sac');
   },
-
   afterModel() {
-    //this.get('amp').set('canonicalUrl', 'http://');
-    //this.get('amp').set('title', 'Works!!');
-    this.get('amp').registerExtension('twitter');
-    set(this, 'headData.title', 'Demo App');
+    const amp = this.get('amp');
+
+    amp.set('canonicalUrl', './');
+    amp.registerExtension('accordion');
+    amp.registerExtension('form');
   }
 });
